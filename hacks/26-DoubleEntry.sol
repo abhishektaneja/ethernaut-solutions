bda21515c69eed0788f8474238085267de4a3728
Abhishek  Taneja <abhishek.taneja@soundcloud.com>
2022-08-17 23:21:52 +0200

    puzzle wallet setup

--- hacks/26-DoubleEntry.sol
+++ hacks/26-DoubleEntry.sol
@@ -22,7 +22,8 @@ In this level you should figure out where the bug is in CryptoVault and protect 
 
 The contract features a Forta contract where any user can register its own detection bot contract. 
 Forta is a decentralized, community-based monitoring network to detect threats and anomalies on DeFi, NFT, governance, bridges and other Web3 systems as quickly as possible. 
-Your job is to implement a detection bot and register it in the Forta contract. The bot's implementation will need to raise correct alerts to prevent potential attacks or bug exploits.
+Your job is to implement a detection bot and register it in the Forta contract.
+ The bot's implementation will need to raise correct alerts to prevent potential attacks or bug exploits.
 
 Things that might help:
 
